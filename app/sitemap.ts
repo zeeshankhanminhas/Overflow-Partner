29741e670f952348bf5c6e874c9a46401896d1b0
zeeshankhanminhas <127130521+zeeshankhanminhas@users.noreply.github.com>
2026-08-02 21:28:05 +0100

    chore: update public site URL branding

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-const siteUrl = 'https://midts.co.uk';
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://overflowpartner.co.uk';
 
 export const dynamic = 'force-static';
 
